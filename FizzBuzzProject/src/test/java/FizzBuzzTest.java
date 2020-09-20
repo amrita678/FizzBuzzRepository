03c612efd709f4b20e810b9912e93318aa21c54c
AMRITA <amritagolu67@gmail.com>
2020-09-20 14:58:40 +0530

    Created FizzBuzz Project

---
+++ FizzBuzzProject/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void shouldReturn1WhenInputIs1() {
+
+	}
+	
+}
