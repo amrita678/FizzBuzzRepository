d882b7e7172960298edf8bea9ec10b48e3757502
AMRITA <amritagolu67@gmail.com>
2020-09-21 18:59:33 +0530

    Taking User input in between 1 to 50

--- FizzBuzzProject/src/main/java/FizzBuzz.java
+++ FizzBuzzProject/src/main/java/FizzBuzz.java
@@ -16,7 +16,14 @@ public class FizzBuzz {
 	}
 	
 	public int checkNumberBetween1To50() {
-				return 0;
+		int userInputNumber=0;
+		Scanner input = new Scanner(System.in);
+		do {
+		    System.out.print("enter a number between 1 to 50 : ");
+		    userInputNumber=input.nextInt();	
+		   }while(userInputNumber<1 || userInputNumber>50);
+
+		return userInputNumber;
 		}
 
 }
